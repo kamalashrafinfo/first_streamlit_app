120d270007a4fcb3777a84a28c2fb4a6cd7aa438
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-05 20:42:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🍇''omega 3 Blueberrry Oatmeal')
 streamlit.text('🍎''Kale, Spinach & Rocket Smothie')
 streamlit.text('🍎''Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
