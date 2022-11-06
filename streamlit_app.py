c40575e825e9a8aac239087b3e7568c6a04c8111
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-05 20:12:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.head('Breakfast Menu')
+streamlit.text('omega 3 Blueberrry Oatmeal')
+streamlit.txt('Kale, Spinach & Rocket Smothie')
