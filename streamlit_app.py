2f1aa793081e67053d9a787058ea4b23c6f9dc20
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-14 20:45:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+#New Section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
