19d25f058e3249b90f555bacf4fb5a0cb16aa4fd
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-14 21:38:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  "apple")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  "kiwi")
 #streamlit.text(fruityvice_response.json()) --Instruction to delete this line.
 
 # write your own comment -what does the next line do? 
