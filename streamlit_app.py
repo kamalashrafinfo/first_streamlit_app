c270a695b7f2b5435a9d7e8daa51eefa397e4d74
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-17 16:48:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ streamlit.text('🍎''Kale, Spinach & Rocket Smothie')
 streamlit.text('🍎''Hard-Boiled Free-Range Egg')
 #import pandas
 #my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
-streamlit.dataframe(my_fruit_list)
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.dataframe(my_fruit_list)
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -27,8 +27,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 #New Section to display fruitvice api response
 #streamlit.header("Fruityvice Fruit Advice!")
 
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-#streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 #import requests
 #fruityvice_response = requests.get(https://fruityvice.com/api/fruit/watermelon)
