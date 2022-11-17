071fa7f4be3e93917dcad30efce4ef541da66de9
Kamal Ashraf <117134751+kamalashrafinfo@users.noreply.github.com>
2022-11-17 12:56:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('🍇''omega 3 Blueberrry Oatmeal')
 streamlit.text('🍎''Kale, Spinach & Rocket Smothie')
 streamlit.text('🍎''Hard-Boiled Free-Range Egg')
 import pandas
-my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+#my_fruit_list = pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
 #streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
